ed9f12f665021aa7b730a82249ad5ceb733bf4ed
allasto <alla@terasky.com>
2022-06-12 14:32:59 +0000

    Added new network for Guarav

--- network.tf
+++ network.tf
@@ -1,6 +1,27 @@
-resource "azurerm_virtual_network" "vnet2" {
-  name                = "${var.prefix}-vnet2"
-  location            = azurerm_resource_group.myresourcegroup.location
-  address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+resource "azurerm_resource_group" "test" {
+  name     = "my-resources"
+  location = "eastus"
 }
+
+module "network" {
+ source  = "app.terraform.io/alla-lab/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = azurerm_resource_group.test.name
+  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    "subnet1" : ["Microsoft.Sql"], 
+    "subnet2" : ["Microsoft.Sql"],
+    "subnet3" : ["Microsoft.Sql"]
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
+}
+
